398caa2c4df324ff598691abf8aacacbc907f1b3
KopIAkash1 <andreymart01@gmail.com>
2025-04-06 12:10:16 +0300

    profile visability added

--- app/src/main/java/com/example/kursach/ProfileFragment.java
+++ app/src/main/java/com/example/kursach/ProfileFragment.java
@@ -1,12 +1,27 @@
 package com.example.kursach;
 
 import android.os.Bundle;
-
+import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +29,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    ImageView avatarIv;
+    TextView nameTv, emailTv, phoneTv;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +81,56 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+
+        avatarIv = view.findViewById(R.id.avatarIv);
+        nameTv = view.findViewById(R.id.nameTv);
+        emailTv = view.findViewById(R.id.emailTV);
+        phoneTv = view.findViewById(R.id.phoneTV);
+
+
+        // ЗАПРОС
+        Log.d("DEBUG FUCK", user.getEmail());
+        Query query = databaseReference.orderByChild("email");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d("DEBUG FUCK", "Children count: " + snapshot.getChildrenCount());
+                Log.d("DEBUG FUCK", "Request maked");
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    String email = "" + ds.child("email").getValue();
+                    if (email.equals(user.getEmail())) {
+                        String name = "" + ds.child("name").getValue();
+                        String phone = "" + ds.child("phone").getValue();
+                        String image = "" + ds.child("image").getValue();
+                        Log.d("DEBUG FUCK", email);
+                        Log.d("DEBUG FUCK", name);
+                        Log.d("DEBUG FUCK", phone);
+                        Log.d("DEBUG FUCK", image);
+
+                        nameTv.setText(name);
+                        emailTv.setText(email);
+                        phoneTv.setText(phone);
+
+                        try {
+                            Picasso.get().load(image).into(avatarIv);
+                        } catch (Exception e) {
+                            Picasso.get().load(R.drawable.ic_add_image).into(avatarIv);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return view;
     }
 }
